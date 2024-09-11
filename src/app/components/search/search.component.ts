8fee2a2f973eef0a0484c1340d8530575c80bfc6
Hazem_Mo <hazemmohamed345674@gmail.com>
2024-09-11 03:42:31 +0300

    enhance search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,33 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService } from '../../services/movie.service';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  stock: number;
+  rating: number;
+  category: string;
+  images : string[];
+}
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule,RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent implements OnInit {
-  searchResult!: any[];
-  private searchVal!: string;
-  constructor(private movServ: MovieService) {}
+  searchResult: Product[] = [];
+  allProducts: Product[] = [];
+  searchValue: string = '';
+
+  constructor(private productService: ProductService) { }
+
   ngOnInit(): void {
-    this.getSearchedMovie('')
+    this.loadProducts();
   }
 
-  set searchValue(val: string) {
-    this.searchVal = val;
-    this.getSearchedMovie(this.searchVal)
-  }
-
-  getSearchedMovie(name: string) {
-    this.movServ.searchByMovieName(name).subscribe((data) => {
-      this.searchResult=data.results
-      console.log(data.results);
-      
+  loadProducts(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (data) => {
+        this.allProducts = data.products;
+        console.log('Products fetched successfully', this.allProducts);
+      },
+      error: (error) => {
+        console.error('Error fetching products', error);
+      }
     });
+  }
+
+  searchProducts(): void {
+    if (this.searchValue.trim() === '') {
+      this.searchResult = [];
+      return;
+    }
+
+    this.searchResult = this.allProducts.filter(product =>
+      product.title.toLowerCase().includes(this.searchValue.toLowerCase())
+    );
   }
 }
